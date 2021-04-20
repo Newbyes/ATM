6b430c496b1c4da2ff87666ecb4c1941711132bf
Pranit5895 <guptapranit9@gmail.com>
2021-04-20 20:23:36 +0530

    While for Options

--- atm/src/atm/Transaction.java
+++ atm/src/atm/Transaction.java
@@ -11,3 +11,13 @@ public class Transaction {
 		System.out.println("Enter Balance in your Account: ");
 		int balance = sc.nextInt();
         Scanner s = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("Automated Teller Machine");
+            System.out.println("Choose 1 for Withdraw");
+            System.out.println("Choose 2 for Deposit");
+            System.out.println("Choose 3 for Check Balance");
+            System.out.println("Choose 4 for EXIT");
+            System.out.print("Choose the operation you want to perform:");
+            int n = s.nextInt();
