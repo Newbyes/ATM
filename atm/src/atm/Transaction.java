09f44217196c062348b1f2aed37bc9c79487f02b
Pranit5895 <guptapranit9@gmail.com>
2021-04-20 20:23:01 +0530

    Initialising and Declaring Objects

---
+++ atm/src/atm/Transaction.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Transaction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int withdraw, deposit;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Balance in your Account: ");
+		int balance = sc.nextInt();
+        Scanner s = new Scanner(System.in);
